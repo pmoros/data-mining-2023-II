0faa1cdd41d46e5a866669ec671c73cae7055744
Paul Moros Anacona <pmoros@outlook.com>
2023-09-21 22:53:25 -0500

    fix: add imports in main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from json_loader import *
+
 if __name__ == "__main__":
     CHUNK_SIZE = 100    
     json_file = "datasets/eval_travel_times_formatted.json"
-    
+
     all_ids = get_all_ids(json_file)
+    print(all_ids)
