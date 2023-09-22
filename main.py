5e286bd9d5e4ca0057b3c69a446db794c0439018
Paul Moros Anacona <pmoros@outlook.com>
2023-09-22 09:00:26 -0500

    feat: read chunks

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
-from json_loader import *
+from io import TextIOBase
+import json
+import re
 
-if __name__ == "__main__":
-    CHUNK_SIZE = 100    
-    json_file = "datasets/eval_travel_times_formatted.json"
+def chunk_read(f_obj: TextIOBase, sentinel: str, max_sentinel: int):
+    """Read a file object in chunks
+    Read the file object line by line. Each time a sentinel is detected, we increment
+    a count. Once the count reaches max_sentinel, we have gatherered the required
+    chunk and yield it.
+    The function is inspired by this SO answer:
+    https://stackoverflow.com/a/42964612/9723036
+    NOTE: during chunking, we remove all the white spaces and tabs to reduce the
+    memory load.
+    :param f_obj: A file object from opening a text file.
+    :type f_obj: TextIOBase
+    :param sentinel: A string pattern (regex supported) to recognize a specific
+        line.
+    :type sentinel: str
+    :param max_sentinel: Max number of appearance of sentinels allowed in a chunk.
+        This is equivalent to a chunk size, but more meaningful than based on only
+        line counts.
+    :type max_sentinel: int
+    :yield: A chunk of the file
+    :rtype: Iterator[str]
+    """
+    cnt, chunk = 0, ''
+    for line in f_obj:
+        match = re.search(sentinel, line)
+        if match:
+            cnt += 1
+        if cnt <= max_sentinel:
+            chunk += line.strip()
+        else:
+            yield chunk
+            cnt = 0
+            chunk = line.strip()
+    yield chunk
 
-    all_ids = get_all_ids(json_file)
-    print(all_ids)
+
+
+if __name__ == '__main__':
+    with open('datasets/eval_travel_times_formatted.json', 'r') as f_obj:
+        for chunk in chunk_read(f_obj, 'RouteID', 1):
+            print('new line:', chunk)
+            break
